312fca648be2ba674dcbb0d6df169eacb52ab97f
MVizba <142807206+MVizba@users.noreply.github.com>
2023-10-26 19:54:17 +0300

    Update sprint3.5.py

--- sprint3.5.py
+++ sprint3.5.py
@@ -31,12 +31,15 @@ class Question:
         self.attempts = 0
         self.correct_attempts = 0
 
+    # Check if question is single or multiple boolen
     def is_single_answer(self):
         return self.options is not None
 
+    # Ceck if question is enable or disable
     def is_enabled(self):
         return bool(int(self.enabled)) 
 
+    # Enable or disable question
     def enable(self, enable):
         self.enabled = enable
     
@@ -63,8 +66,10 @@ class Question:
             for i, option in enumerate(self.options, 1):
                 print(f"{i}. {option}")
 
+        #Check if answeer is correct or not
     def check_answer(self, user_answer):
         self.attempts += 1
+        print(self.is_single_answer())
         try:
             if self.is_single_answer():
                 is_correct = user_answer.lower() == self.correct_answer.lower()
@@ -87,7 +92,7 @@ class Question:
             answer = self.options[answer_index]
         return answer
 
-
+    # COnvert a question to csv format
     def to_csv(self):
         if self.is_single_answer():
             return [self.id, self.enabled, self.text, "SingleStringAnswer", self.correct_answer, self.attempts, self.correct_attempts]
@@ -95,6 +100,7 @@ class Question:
             return [self.id, self.enabled, self.text, "MultipleChoice", *self.options, str(self.correct_answer), self.attempts, self.correct_attempts]
 
     @classmethod
+    # Create a question object from CSV data
     def from_csv(cls, row):
         question_type = row[3]
         if question_type == "SingleStringAnswer":
@@ -119,7 +125,6 @@ class SingleStringAnswerQuestion(Question):
     def is_single_answer(self):
         return True
 
-
 class MultipleChoiceQuestion(Question):
     def __init__(self, uuid, enabled, text, correct_answer, options):
         super().__init__(uuid, enabled, text, correct_answer, options)
@@ -141,7 +146,6 @@ def load_questions(filename):
         reader = csv.reader(file)
         for row in reader:
             questions.append(Question.from_csv(row))
-    random.shuffle(questions)
     return questions
 
 def main():
@@ -154,6 +158,7 @@ def main():
             print("3. Disable/Enable questions")
             print("4. Practice mode")
             print("5. Test mode")
+            print("6. For exiting the program")
 
             choice = int(input("Enter your choice: "))
 
@@ -162,13 +167,15 @@ def main():
                 if question_type == 1:
                     answers = []
                     question_text = str(input("Enter your question: "))
-                    question_answer = str(input("Enter answer or . to end"))
+                    
+                    for _ in range(3):      # Added range to limit answer input
+                        question_answer = str(input("Enter answer or . to end "))
+                        if question_answer == "." or len(answers) >= 3:
+                            break
             
-                    while question_answer != ".":
+                    
                         answers.append(question_answer)
-                        question_answer = str(input("Enter answer or . to end: "))
-
-                    print(answers)
+                        
 
                     correct_answer = int(input("Which answer is correct? Enter number:"))
                     correct_answer = correct_answer - 1
@@ -177,6 +184,8 @@ def main():
                     questions.append(MultipleChoiceQuestion(unique_id, 1, question_text, correct_answer, answers))
                     save_questions(questions, "questions.csv")
 
+                    print("\nQuestion added to .csv file! ") # Added message that question is added
+
                 elif question_type == 2:
                     question_text = str(input("Enter your question: "))
                     question_answer = str(input("Enter answer: "))
@@ -184,6 +193,8 @@ def main():
 
                     questions.append(SingleStringAnswerQuestion(unique_id, 1, question_text, question_answer))
                     save_questions(questions, "questions.csv")
+
+                    print("\nQuestion added to .csv file! ") # Added message that question is added
 
                 else:
                     print("wrong choice ")
@@ -195,7 +206,7 @@ def main():
                     print("\r")
                     
                     unique_id = str(question.get_uuid())
-                    print("ID:" + unique_id + " Enabled: " + str(question.is_enabled()))
+                    print("UUID:" + unique_id + " Enabled: " + str(question.is_enabled()))
                     
                     question.display()
                     attempts = str(question.get_attempts())
@@ -213,10 +224,10 @@ def main():
                         print("ID:" + unique_id + " Enabled: " + str(question.is_enabled()))
                         question.display()
                         print("Answer: " + question.get_correct_answer())
-                        question_enable = str(input("Enter, enable or disable: "))
-                        if question_enable == "enable":
+                        question_enable = str(input("Enter, 1 for enable or 2 for disable: ")) # Changed to 1 - enable and 2 for disable
+                        if question_enable == "1": #
                             question.enable(1)
-                        elif question_enable == "disable":
+                        elif question_enable == "2":#
                             question.enable(0)
                         else:
                             print("wrong input, try again: ")
@@ -232,11 +243,12 @@ def main():
                     continue
 
                 while True:
-                    weights = [question.correct_attempts for question in questions]
+                    weights = [question.correct_attempts for question in questions]   # Changed to correct attepts
                     question = random.choices(questions, k=1, weights=weights)[0]
                 
                     if question.is_enabled() == False: 
                         continue
+
                     question.display()
                     user_answer = input("Your answer: ")
                     if user_answer == ".":
@@ -258,18 +270,27 @@ def main():
                     print("You need to add more questions to start.")
                     continue
 
-                print(f"maximum number of possible questions to answer is {num_questions}")
+                enabled_questions = []      # Changed to get list of ennabled   question to choose for answering
+                for question in questions:
+                    if question.is_enabled() == True:
+                        enabled_questions.append(question)
+                num_enabled_questions = len(enabled_questions)
+
+                print(f"maximum number of possible questions to answer is {num_enabled_questions}")
                 num_to_answer = int(input("How many questions will you answer? "))
-                if num_to_answer > num_questions:
+                if num_to_answer > num_enabled_questions:
                     print("Choose lower number of questions!")
                     continue
 
                 correct_answers = 0
 
-                for question in random.sample(questions, num_to_answer):
-                    if question.is_enabled() == False: 
-                        continue
-                    
+                enabled_questions = []             #Changed to enabled question
+                for question in questions:
+                    if question.is_enabled() == True:
+                        enabled_questions.append(question)
+
+                for question in random.sample(enabled_questions, num_to_answer): # Changed to enabled questions
+
                     question.display()
                     user_answer = input("Your answer: ")
                     is_correct = question.check_answer(user_answer)
@@ -284,6 +305,10 @@ def main():
                     results_file.write(f"{score} - {current_time}\n")
                 print(score)
                 save_questions(questions, "questions.csv") 
+        
+            elif choice == 6:
+                print("Goodbye!")
+                sys.exit()  # Exit the program
 
             else:
                 Print("Invalid choice.Please try again. ")
@@ -291,16 +316,19 @@ def main():
 if __name__ == "__main__":
     main()
 
-    
+# change disable=enable to 1 and 2. 
+# change quite choice 6.  
+# and print (message in "question was added.")
+# 
 
     
     
 # Link to GitHub hands-on: https://github.com/MVizba/war_game 
-# Didn't finnished this task, but: 
+# Didn't finish this task, but: 
 # README.md file was created 
-# Repository was cloned to PC using terminal. 
-# through terminal README.md file was updated.abs
-# through terminal war.py was uploaded to repository. 
-# Issues was created for this card game for further coding.
-# 2 issues was solved by me. Uploaded throw terminal from my PC. - git add war.py and git commit -m, git merge and git push to update GitHub
-# With pear I will finish this task. He also didn't finnish. And due to deadline there's no time to do it. 
+# Repository was cloned to PC using the terminal. 
+# Through the terminal README.md file was updated.abs
+# through terminal war.py was uploaded to the repository. 
+# Issues were created for this card game for further coding.
+# 2 issues were solved by me. Uploaded through the terminal from my PC. - git add war.py and git commit -m, git merge and git push to update GitHub
+# With pear, I will finish this task. He also didn't finish. And due to the deadline, there's no time to do it. 
